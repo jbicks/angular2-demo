f348a255397324c64050a3b35c7a4bc81b0e10e7
Colin Kirk <colin.kirk@gmail.com>
2016-05-03 20:25:39 -0600

    added missing CourseItemComponent

---
+++ app/components/course-item/course-item.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: "course-item",
+  template: `<div>course item component placeholder</div>`
+})
+export class CourseItemComponent {
+
+}
--- app/components/course-list/course-list.component.ts
+++ app/components/course-list/course-list.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {CourseItemComponent} from '../course-item/course-item.component'
 
 @Component({
   selector: "course-list",
-  template: `<div>course list component placeholder</div>`
+  template: `<div>course list component placeholder</div>`,
+  directives: [CourseItemComponent]
 })
 export class CourseListComponent {
 
