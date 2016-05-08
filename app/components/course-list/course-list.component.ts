4189e09e9f4eb253741ff73b0e668ceca28e552b
Justin Bicknell <justin.bicknell@JBICKNELL-WIN.BlatantMedia.ad>
2016-05-08 12:02:11 -0600

    initial routing - just login and courses

--- app/components/course-list/course-list.component.ts
+++ app/components/course-list/course-list.component.ts
@@ -2,10 +2,11 @@ import {Component} from 'angular2/core';
 import {CourseItemComponent} from '../course-item/course-item.component'
 
 @Component({
-  selector: "course-list",
   template: `<div>course list component placeholder</div>`,
   directives: [CourseItemComponent]
 })
 export class CourseListComponent {
-
+  constructor(){
+    console.log('in constructor')
+  }
 }
--- app/services/constants/environment.ts
+++ app/services/constants/environment.ts
@@ -1,6 +1,6 @@
 
 export const Environment = {
-    baseUrl: 'http://localhost:58532/api/rest/v1/',
+    baseUrl: 'http://localhost:2000/api/rest/v1/',
     apiKey: '157f8bd9-1e68-4f41-baf6-c94c2171a4df',
     endpoints: {
       authenticate: 'authenticate?username={USERNAME}&password={PASSWORD}&privateKey={PRIVATE_KEY}',
