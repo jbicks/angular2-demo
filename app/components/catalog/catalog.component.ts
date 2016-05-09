cfec6a6932440b374f4f30960704442318b87635
Justin Bicknell <justin.bicknell@JBICKNELL-WIN.BlatantMedia.ad>
2016-05-08 20:00:58 -0600

    fix logout

--- app/components/catalog/catalog.component.ts
+++ app/components/catalog/catalog.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {CourseListComponent} from '../course-list/course-list.component';
 import {FilterSelectorComponent} from '../filter-selector/filter-selector.component';
 import {DropdownComponent} from '../dropdown/dropdown.component';
 import {ProfileComponent} from '../profile/profile.component';
+import {StorageService} from '../../services/storage/storage.service';
 
 @Component({
     selector: "catalog",
@@ -17,6 +19,11 @@ import {ProfileComponent} from '../profile/profile.component';
     directives: [CourseListComponent, FilterSelectorComponent, DropdownComponent, ProfileComponent]
 })
 export class CatalogComponent {
+    constructor(private _router: Router, 
+                private _storageService:StorageService){
+                    
+                }
+                
     logout(){
             this._storageService.clear();
             this._router.navigate(["Login"]);
