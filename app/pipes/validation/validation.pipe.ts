efdb10461b95c5858f0ccd2b951fb081482269b3
Colin Kirk <colin.kirk@absorblms.com>
2016-05-08 19:47:40 -0600

    - added first test

--- app/pipes/validation/validation.pipe.ts
+++ app/pipes/validation/validation.pipe.ts
@@ -1,20 +1,22 @@
 import {Pipe, PipeTransform} from 'angular2/core'
 
+
+
 @Pipe({
     name: 'validation'
 })
 export class ValidationPipe implements PipeTransform {
 
+    static REQUIRED:string = 'Required.';
+    static INVALID_CREDENTIALS:string = 'Incorrect username and/or password.';
+
     transform(value: any, args: any[]) {
         if (value) {
             if (value.required) {
-                return "Required.";
+                return ValidationPipe.REQUIRED;
             }
             if (value.invalidCredentials) {
-                return "Incorrect username and/or password.";
-            }
-            if (value.userDetailsFailure) {
-                return "Failed to load user details.";
+                return ValidationPipe.INVALID_CREDENTIALS;
             }
         }
         return null;
